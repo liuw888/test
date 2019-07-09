5acee890094b3be1e0d2242442937c5546ff7a8e
liuw888 <52521917+liuw888@users.noreply.github.com>
2019-07-09 13:57:21 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,16 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import os
+import sys
+from bs4 import BeautifulSoup
+import lxml.html as lh
+import datetime
+from graphviz import Digraph
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
