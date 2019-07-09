0a9d4d175e98b2621bcbcc7835c6ff9ec8500fe9
liuw888 <52521917+liuw888@users.noreply.github.com>
2019-07-09 11:30:04 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template    
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/hi")
+def hi():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
