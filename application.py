e8df14c24b0988b29efa46c24f8b45b3e6cd329b
liuw888 <52521917+liuw888@users.noreply.github.com>
2019-07-09 16:48:17 +0800

    c

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ from flask import Flask, request, redirect, url_for, render_template, send_from_
 import datetime
 from bs4 import BeautifulSoup
 from graphviz import Digraph
+import pybase64
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
 DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
@@ -332,7 +333,14 @@ def index():
                 app.config['UPLOAD_FOLDER'], filename), filename)
             return redirect(url_for('uploaded_file', filename=filename))
     print('App works', file=sys.stderr)
-    return render_template('index.html')
+    chart_data = Digraph()
+
+    chart_data.node('H', 'Hello')
+    chart_data.node('W', 'World')
+    chart_data.edge('H', 'W')
+    chart_output = chart_data.pipe(format='png')
+    chart_output = pybase64.b64encode(chart_output).decode('utf-8')
+    return render_template('index.html',chart_output=chart_output)
 
 
 @app.route("/hi")
