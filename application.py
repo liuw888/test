224245da706c353469c852d964ffc7d78020baf2
liuw888 <52521917+liuw888@users.noreply.github.com>
2019-07-09 13:49:01 +0800

    hi

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("home.html")
 
 @app.route("/hi")
 def hi():
     return "Hi World!"
 
+if __name__ == "__main__":
+    app.run(debug=True)
