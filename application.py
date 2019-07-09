277a7243f2c91b86f656b4a7d9c9abb4f529ea95
liuw888 <52521917+liuw888@users.noreply.github.com>
2019-07-09 14:26:46 +0800

    Update application.py

--- application.py
+++ application.py
@@ -7,9 +7,25 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template("index.html")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('No file attached in request', file=sys.stderr)
+            return redirect(request.url)
+        file = request.files['file']
+        print(file, file=sys.stderr)
+        if file.filename == '':
+            print('No file selected', file=sys.stderr)
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            process_file(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename), filename)
+            return redirect(url_for('uploaded_file', filename=filename))
+    print('App works', file=sys.stderr)
+    return render_template('index.html')
 
 @app.route("/hi")
 def hi():
