e9153ca136632950a7a8c5b8e19d77077ee35e38
liuw888 <52521917+liuw888@users.noreply.github.com>
2019-07-10 10:55:07 +0800

    Update application.py

--- application.py
+++ application.py
@@ -4,6 +4,13 @@ from werkzeug.utils import secure_filename
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory, flash
 import datetime
 from bs4 import BeautifulSoup
+from graphviz import Digraph
+import pybase64
+
+
+
+app = Flask(__name__, static_url_path="/static")
+
 
 
 
@@ -11,7 +18,13 @@ from bs4 import BeautifulSoup
 def index():
     print('app works')
   
-    return 'Hello World!'
+    chart_data = Digraph()
+    chart_data.node('H', 'Hello')
+    chart_data.node('W', 'World')
+    chart_data.edge('H', 'W')
+    chart_output = chart_data.pipe(format='png')
+    chart_output = pybase64.b64encode(chart_output).decode('utf-8')
+    return render_template('index.html',chart_output=chart_output)
 
 
 @app.route("/hi")
