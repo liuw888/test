907edc74d49a35bdc4eb6690b59cb12ca5664f75
liuw888 <52521917+liuw888@users.noreply.github.com>
2019-07-11 16:19:35 +0800

    Update application.py

--- application.py
+++ application.py
@@ -155,7 +155,7 @@ def post():
                 res += chr(j+65) + ''' [label=< 
                 <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
                         <tr >
-                            <td colspan ="4" bgcolor='%s'>Completed</td>
+                            <td colspan ="4" bgcolor='%s'><img src=https://github.com/liuw888/test/blob/master/ok.png></img></td>
                         </tr>
                         <tr>
                             <td rowspan ="4" colspan ="3" >Yes</td>
